6c5c6fdf3a198a8f36e013edd3db836d1ea8a022
Elisha Gerson <egerson96@gmail.com>
2020-11-16 21:30:29 +0200

    added server manager class

---
+++ ServerManager.cpp
@@ -0,0 +1,5 @@
+//
+// Created by elisha on 11/16/20.
+//
+
+#include "ServerManager.hpp"
---
+++ ServerManager.hpp
@@ -0,0 +1,12 @@
+//
+// Created by elisha on 11/16/20.
+//
+
+#pragma once
+
+class ServerManager {
+
+};
+
+
+#endif //WEBSERVER_SERVERMANAGER_HPP
